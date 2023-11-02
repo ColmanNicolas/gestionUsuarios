f5e89914add02930eb6817ff0b5e08fa3b594869
ColmanNicolas <colman.nicolas.17@hotmail.com>
2023-11-02 00:42:02 -0300

    todas las funcionalidades trabajando con funciones(falta comentar )

--- js/app.js
+++ js/app.js
@@ -1,57 +1,65 @@
-let usuarios = [];
 
-function manejarFormulario(event) {
+function manejarImputFormularioDeUsuarios(event) {
   event.preventDefault();
-
   const correo = document.getElementById("inputEmail");
   const nombre = document.getElementById("inputName");
-
-  // aca quiero agregar un elemento al array
   const usuario = {
     nombre: nombre.value,
     correo: correo.value,
   };
 
-  // agrego un elemento al array
-  usuarios.push(usuario);
-
-  // guardo el array completo dentro del LS
-  localStorage.setItem("listaUsuarios", JSON.stringify(usuarios));
-  cargarLS();
-  nombre.value = "";
-  correo.value = "";
-  correo.focus();
+  ingresarElementoEnArray("listaUsuarios",usuario);
+nombre.value="";
+correo.value="";
+correo.focus();
 }
 
-function borrarUsuario(index) {
-  console.log("Hiciste un click en el boton Eliminar", index);
-  usuarios.splice(index, 1);
-  localStorage.setItem("listaUsuarios", JSON.stringify(usuarios));
-  cargarLS();
+function ingresarElementoEnArray(listaDeLS,elemento){
+console.log("aca llega el elemento:" ,elemento);
+ let elementosArray = obtenerContenidoLS(listaDeLS);
+ console.log(elemento);
+  elementosArray.push(elemento);
+  // envio el array completo a la funcion para actualizar LS
+  actualizarArrayLS(listaDeLS,elementosArray);
+  crearTablaDeElementos(listaDeLS);
 }
 
-function cargarLS() {
-  // ver si hay algo en el LS
-  const listaUsuarios = localStorage.getItem("listaUsuarios");
-  if (listaUsuarios) {
-    usuarios = JSON.parse(listaUsuarios);
-  } else {
-    usuarios = [];
+
+//ya esta
+function actualizarArrayLS(listaDeLS,elementosArray){
+    localStorage.setItem(listaDeLS, JSON.stringify(elementosArray));
+}
+
+function obtenerContenidoLS(nombreLista) {
+  // Ver si hay algo en el LS
+  let elementosArray = [];
+  const listaDeLS = localStorage.getItem(nombreLista);
+  if (listaDeLS) {
+    elementosArray = JSON.parse(listaDeLS);
   }
+  return elementosArray;
+}
+
+function borrarElemento(index,nombreLista) {
+  let elementosArray = obtenerContenidoLS(nombreLista);
+  elementosArray.splice(index, 1);
+  actualizarArrayLS(nombreLista,elementosArray);
+  crearTablaDeElementos(nombreLista);
+}
   // dibujar en en Table sus elementos
+function  crearTablaDeElementos (nombreLista){
   const tabla = document.getElementById("tablaUsuarios");
   tabla.innerHTML = "";
-
-  usuarios.forEach((element, index) => {
+  arrayElementos = obtenerContenidoLS(nombreLista);
+  arrayElementos.forEach((element, index) => {
     tabla.innerHTML += `
     <tr>
       <th scope="row">${index + 1}</th>
       <td>${element.nombre}</td>
       <td>${element.correo}</td>
-      <td><button class="btn btn-danger" onclick="borrarUsuario(${index})">Eliminar</button></td>
-    </tr>
+      <td><button class="btn btn-danger" onclick="borrarElemento(${index}, 'listaUsuarios')">Eliminar</button></td>  </tr>
     `;
   });
 }
 
-cargarLS();
+crearTablaDeElementos("listaUsuarios");
